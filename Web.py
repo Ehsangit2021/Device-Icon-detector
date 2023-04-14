a30d58a7a968626c605929e2d91bb414ab10a51c
ehsangit2021 <ehsan.aval87@gmail.com>
2023-04-15 00:00:02 +0430

    No Error

--- Web.py
+++ Web.py
@@ -19,13 +19,24 @@ img1 = st.file_uploader("Select your input file", type=["jpg"])
 
 pytesseract.pytesseract.tesseract_cmd = 'C:/Users/Ethan/AppData/Local/Programs/Tesseract-OCR/tesseract.exe'
 if img1:
+
     img = Image.open(img1)
+
+    result = pytesseract.image_to_string(img)
+    matrices = result.split('\n')
+    counter=0
+    for i in matrices:
+        if 'VB' in i:
+            counter += 1
+    st.write('Counter of device including VB in their name is: ', counter)
+
     texts = pytesseract.image_to_data(img)
     texts2 = texts.replace('\t', ',')
 
     temp = list(texts.split('\n'))
-    [temp[i].split('\t') for i,j in enumerate(temp)]
+    # [temp[i].split('\t') for i,j in enumerate(temp)]
     temp2 = pd.DataFrame([temp[i].split('\t') for i,j in enumerate(temp)])
     st.write(pd.DataFrame(temp2))
     pd.DataFrame(temp2).to_csv('text.csv')
+
 
