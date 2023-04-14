512f9f45ef7e72060ccecb4513a149d2d6c9193a
ehsangit2021 <ehsan.aval87@gmail.com>
2023-04-15 00:18:59 +0430

    Now it accepts the input P&ID image then find the text remove Nan and show to the end-user.

--- Web.py
+++ Web.py
@@ -2,10 +2,11 @@ import streamlit as st
 from PIL import Image
 import pytesseract
 import pandas as pd
+import numpy as np
 
 im = Image.open("./favicon.ico")
 st.set_page_config(
-    page_title=" P&ID Icons",
+    page_title=" P&ID Quest",
     page_icon=im,
     layout="wide"
 )
@@ -18,25 +19,29 @@ img1 = st.file_uploader("Select your input file", type=["jpg"])
 
 
 pytesseract.pytesseract.tesseract_cmd = 'C:/Users/Ethan/AppData/Local/Programs/Tesseract-OCR/tesseract.exe'
-if img1:
 
-    img = Image.open(img1)
+with st.spinner('Wait for it...'): 
+    if img1:
 
-    result = pytesseract.image_to_string(img)
-    matrices = result.split('\n')
-    counter=0
-    for i in matrices:
-        if 'VB' in i:
-            counter += 1
-    st.write('Counter of device including VB in their name is: ', counter)
+        img = Image.open(img1)
 
-    texts = pytesseract.image_to_data(img)
-    texts2 = texts.replace('\t', ',')
+        result = pytesseract.image_to_string(img)
+        matrices = result.split('\n')
+        counter=0
+        for i in matrices:
+            if 'VB' in i:
+                counter += 1
+        st.success(f'Counter of device including VB in their name is: {counter}')
 
-    temp = list(texts.split('\n'))
-    # [temp[i].split('\t') for i,j in enumerate(temp)]
-    temp2 = pd.DataFrame([temp[i].split('\t') for i,j in enumerate(temp)])
-    st.write(pd.DataFrame(temp2))
-    pd.DataFrame(temp2).to_csv('text.csv')
+        texts = pytesseract.image_to_data(img)
+        texts2 = texts.replace('\t', ',')
+
+        temp = list(texts.split('\n'))
+        temp2 = pd.DataFrame([temp[i].split('\t') for i,j in enumerate(temp)])
+        temp2.iloc[:,11].replace('', np.nan, inplace=True)
+        temp2.dropna(inplace=True)
+
+        st.write(pd.DataFrame(temp2))
+        pd.DataFrame(temp2).to_csv('text.csv')
 
 
